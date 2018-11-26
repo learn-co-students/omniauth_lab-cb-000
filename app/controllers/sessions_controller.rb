7612612aa195787bc45d88c122ed622c5f131f32
Adam Urbina <adamurbina@gmail.com>
2018-11-26 13:55:54 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,11 @@
 class SessionsController < ApplicationController
 
   def create
+
     auth_hash = request.env['omniauth.auth']
 
-    user = User.find_by(uid: auth_hash['uid'])
-
-    if !user.nil?
-      session[:user_id] = user.id
-    else
-      user = User.new(name: auth_hash[:info][:name], email: auth_hash[:info][:email], uid: auth_hash[:uid])
-      user.save
-      session[:user_id] = user.id
-    end
+    user = User.find_or_create_by_auth(auth_hash)
+    session[:user_id] = user.id
 
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
-  
+
+  def self.find_or_create_by_auth(auth_hash)
+
+    self.where(uid: auth_hash[:uid]).first_or_create do |user|
+      user.provider = auth_hash[:provider]
+      user.email = auth_hash[:info][:email]
+      user.name = auth_hash[:info][:name]
+    end
+  end
+
 end
