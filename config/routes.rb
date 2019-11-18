11c2bac329c290186e6d00a625a6d56b1c222e9f
Will Hodgson <will.hodgson@current-rms.com>
2019-11-18 14:00:50 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # Add your routes here
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+
+  get '/home', to: 'welcome#home'
+  root to: 'welcome#home'
 end
